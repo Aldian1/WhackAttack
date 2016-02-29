a9f618b142e1c46028135bd3d9552fe1591fb9ae
Aldian1 <Aldian@hotmail.co.uk>
2016-02-29 23:32:28 +0000

    Big Update Added:
    -Networking
    	-Room Creation
    	-NetoworkSmoothing
    -Game
    	-Player
    	-Connection Feedback
    -Accounts
    	-Account UI

---
+++ UnityFiles/New Unity Project/Assets/NetworkSmoother.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkSmoother : Photon.MonoBehaviour {
+
+    Vector3 realpos;
+    Quaternion realrot;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(photonView.isMine)
+        { }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, realpos, .1F);
+            transform.rotation = Quaternion.Lerp(transform.rotation, realrot, .1F);
+        }
+	}
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            realpos = (Vector3)stream.ReceiveNext();
+            realrot = (Quaternion)stream.ReceiveNext();
+
+        }
+    }
+}
---
+++ UnityFiles/New Unity Project/Assets/_NetworkManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class _NetworkManager : MonoBehaviour {
+
+
+    public GameObject ConnectionDetails;
+
+    public GameObject player;
+
+    public Transform spawn;
+    // Use this for initialization
+	void Start () {
+        Connect();
+	}
+	
+	void Update()
+    {
+        ConnectionDetails.GetComponent<Text>().text = "Connection Details: " + PhotonNetwork.connectionStateDetailed.ToString();
+
+    }
+
+
+    void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+
+    }
+
+
+    public void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+
+    }
+
+
+
+        void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("Joined failed");
+        PhotonNetwork.CreateRoom(null);
+
+    }
+
+    void OnJoinedRoom()
+    {
+
+        Debug.Log("Joined Room");
+        SpawnPlayer();
+    }
+
+    void SpawnPlayer()
+    {
+        Debug.Log("runonce");
+       GameObject go = PhotonNetwork.Instantiate("Player", spawn.position, Quaternion.identity,0) as GameObject;
+
+        go.GetComponent<_Player>().enabled = true;
+        go.AddComponent<Rigidbody>();
+        go.GetComponent<Rigidbody>().freezeRotation = true;
+
+    }
+}
